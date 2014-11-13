fea91c6a9b519abebc4d05958ad300c9603080a3
giulio luzzati <giulio.luzzati@edu.unige.it>
2014-11-13 17:12:29 +0100

    Working Benchmarks

--- src/dsp/unige/OpenRQTest/MainClass.java
+++ src/dsp/unige/OpenRQTest/MainClass.java
@@ -1,13 +1,6 @@
 package dsp.unige.OpenRQTest;
 
 import java.awt.BorderLayout;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.nio.charset.Charset;
-import java.util.HashMap;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,6 +8,7 @@ import javax.swing.JPanel;
 
 import dsp.unige.ALC.utils.Constants;
 import dsp.unige.alc.benchmark.JpegCoderBenchmark;
+import dsp.unige.alc.benchmark.RQCoderBenchmark;
 
 
 public class MainClass {
@@ -24,20 +18,49 @@ public class MainClass {
 	static JLabel lRx;
 	public static void main(String[] args) {
 
-		benchmarkJpeg();
+		doBenchmarks();
+
 	}
-	
+
+	private static void doBenchmarks() {
+		Thread jT = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				benchmarkJpeg();
+			}
+		});
+		jT.start();
+
+		Thread rqT = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				benchmarkRQ();		
+			}
+		});
+		rqT.start();
+		
+		try {
+			jT.join();
+			rqT.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	public static void benchmarkJpeg() {
 		JpegCoderBenchmark jcb = new JpegCoderBenchmark();
 		jcb.go();
 	}
 
 
-	
 	public static void benchmarkRQ(){
-		
+		RQCoderBenchmark rcb = new RQCoderBenchmark();
+		rcb.go();
 	}
-	
+
 	public static void testRX_and_TX(){
 		frame = new JFrame("");
 		panel = new JPanel(new BorderLayout());
@@ -52,46 +75,48 @@ public class MainClass {
 
 		System.out.println("MainClass.main() RX start");
 		(new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				RxTest rtest = new RxTest();
 				rtest.go(lRx);				
 			}
 		})).start();
-	
-		
+
+
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("MainClass.main() TX start");
 		TxTest ttest = new TxTest();
 		ttest.go(lTx);
 	}
-	
+
 	public static void testJpeg(){
-		
+
 		JPEGTest t2 = new JPEGTest();
-			t2.go();
+		t2.go();
 		System.out.println("JPEG codec ok");
 	}
-	
+
 	public static void testRQeg(){
 		RQTest t1 = new RQTest();
-		
-//		double errc=0;
-//		int ITERATIONS = 10;
+
+		//		double errc=0;
+		//		int ITERATIONS = 10;
 		t1.go(20,0.3);
 		System.out.println("RQ codec ok");
-//		for(int k=34;k>0;k--){
-//			errc=0;
-//			for(int i=0;i<ITERATIONS;i++)
-//				errc+=t1.go(k,0.3);
-//			errc=errc/ITERATIONS;
-//			System.out.println("Raptor codec ok:"+String.format("%6.2f",((double)k/35))+" errc: "+errc);
-//		}
+
+
+		//		for(int k=34;k>0;k--){
+		//			errc=0;
+		//			for(int i=0;i<ITERATIONS;i++)
+		//				errc+=t1.go(k,0.3);
+		//			errc=errc/ITERATIONS;
+		//			System.out.println("Raptor codec ok:"+String.format("%6.2f",((double)k/35))+" errc: "+errc);
+		//		}
 	}
 }
--- src/dsp/unige/alc/benchmark/RQCoderBenchmark.java
+++ src/dsp/unige/alc/benchmark/RQCoderBenchmark.java
@@ -1,5 +1,78 @@
 package dsp.unige.alc.benchmark;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+
+import dsp.unige.ALC.utils.Constants;
+import dsp.unige.OpenRQTest.RQTest;
+
 public class RQCoderBenchmark {
+
+	private static final double CWLEN = Constants.CWLEN;
+
+	public void go() {
+		double pl;
+		double[] results;
+
+		File csv =  new File("benchmarks/plr_rc.csv");
+		OutputStreamWriter osw = null;
+
+		try {
+			osw =  new OutputStreamWriter(new FileOutputStream(csv),Charset.forName("UTF-8"));
+			String title;
+			title = "PL;";
+			for(int k=34;k>0;k--){
+				title+=String.format("%01.4f",(double)k/CWLEN)+";";
+			}
+			title+="\n";
+			osw.write(title);
+			
+
+			for(pl = 0; pl< 1; pl+=0.01){
+				osw.write(String.format("%01.6f",pl)+";");
+				results = partial(pl);
+				for (double d : results) {
+					System.out.print(String.format("%01.6f",d)+" ");
+					osw.write(String.format("%01.6f",d)+";");
+				}
+				System.out.println("");
+				osw.write("\n");
+				osw.flush();
+			}
+
+			osw.flush();
+			osw.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private double[] partial(double pl) {
+		RQTest t1 = new RQTest();
+
+		double []errc=new double[34];
+		int ITERATIONS = 1000;
+		int informationSymbols;
+
+		for(int k=33;k>=0;k--){
+			errc[k]=0;
+			informationSymbols = k+1;
+			for(int i=0;i<ITERATIONS;i++)
+				errc[k]+=t1.go(informationSymbols,pl);
+			errc[k]=errc[k]/ITERATIONS/CWLEN;
+			//			System.out.println("PLoss = "+pl+", Rc="+String.format("%6.2f",((double)k/35))+" errc: "+errc);
+		}		
+		return errc;
+	}
+
+
+
 
 }
