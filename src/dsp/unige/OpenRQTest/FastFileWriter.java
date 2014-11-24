9bf14744bdbc744be006f42d44cf94d06a9e34eb
giulio luzzati <giulio.luzzati@edu.unige.it>
2014-11-24 10:08:02 +0100

    parser buono aggiunto

---
+++ src/dsp/unige/OpenRQTest/FastFileWriter.java
@@ -0,0 +1,26 @@
+package dsp.unige.OpenRQTest;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FastFileWriter extends FileWriter {
+
+	char[] buffer =  new char[100000];
+	int pos = 0;
+	
+	public FastFileWriter(File file) throws IOException {
+		super(file);
+	}
+	@Override
+	public void write(String str) throws IOException {
+		str.getChars(0, str.length(), buffer, pos);
+		pos +=str.length();
+	}
+	
+	@Override
+	public void close() throws IOException {
+		super.write(buffer,0,pos);
+		super.close();
+	}
+}
